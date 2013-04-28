dabfa268c87deb1f7e97f46a08fb4f1756eb9eab
Adrian Barabino <adrian.barabino@gmail.com>
2013-04-28 06:20:16 +0400

    Languages

---
+++ site.js
@@ -0,0 +1,12 @@
+$(document).on("ready", start);
+
+function start () {
+	$(".lang").on("click", changeLang);
+}
+
+function changeLang (info) {
+	id = currentTarget.id;
+	if(id == "es"){
+		$("header p").html("Tengo 17 años, soy un aficionado de la fotografía, desarrollo web y el periodismo. Nací un 5 de Agosto de 1995 en Río Gallegos, Santa Cruz, Argentina, donde vivo actualmente. En el 2007 hice mis primeros pasos en el desarrollo web. <br>Hoy trabajo en esto como Freelancer, haciendo trabajos en desarrollo Backend y Frontend. Me dedico especialmente a la programación web.");
+	}
+}
