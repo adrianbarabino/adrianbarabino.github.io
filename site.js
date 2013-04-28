fa54a0b2f23b77b139f0b07127b3825480d14a62
Adrian Barabino <adrian.barabino@gmail.com>
2013-04-28 07:11:21 +0400

    lang fix

--- site.js
+++ site.js
@@ -1,15 +1,21 @@
-$(document).on("ready", start);
-
 function start () {
 	$(".lang").on("click", changeLang);
+	$("[data-translate]").jqTranslate('index');
 }
+
+$(document).on("ready", start);
+
+
 
 function changeLang (info) {
 	id = info.currentTarget.id;
 	if(id == "es"){
-		$("header h2").html("Desarrollador Web / Fotógrafo");
-		$("header p").html("Tengo 17 años, soy un aficionado de la fotografía, desarrollo web y el periodismo. Nací un 5 de Agosto de 1995 en Río Gallegos, Santa Cruz, Argentina, donde vivo actualmente. En el 2007 hice mis primeros pasos en el desarrollo web. <br>Hoy trabajo en esto como Freelancer, haciendo trabajos en desarrollo Backend y Frontend. Me dedico especialmente a la programación web.");
-		$("h2.social").html("En las redes sociales");
-		$("h2.jobs").html("Mis trabajos");
+		$("[data-translate]").jqTranslate('index', {
+			forceLang: "es"
+		});
+	}else{
+		$("[data-translate]").jqTranslate('index', {
+			forceLang: "en"
+		});		
 	}
 }
