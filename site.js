b7d2ed0b62de36599de801ede9529dbede088b71
Adrian Barabino <adrian.barabino@gmail.com>
2013-04-28 06:21:29 +0400

    error en el site.js

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ function start () {
 }
 
 function changeLang (info) {
-	id = currentTarget.id;
+	id = info.currentTarget.id;
 	if(id == "es"){
 		$("header p").html("Tengo 17 años, soy un aficionado de la fotografía, desarrollo web y el periodismo. Nací un 5 de Agosto de 1995 en Río Gallegos, Santa Cruz, Argentina, donde vivo actualmente. En el 2007 hice mis primeros pasos en el desarrollo web. <br>Hoy trabajo en esto como Freelancer, haciendo trabajos en desarrollo Backend y Frontend. Me dedico especialmente a la programación web.");
 	}
