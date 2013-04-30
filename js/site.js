38ce5ded820f5e1c2b29512f34fd85999a227811
Adrian Barabino <adrian.barabino@gmail.com>
2013-04-30 10:51:24 +0400

    add overlay

--- js/site.js
+++ js/site.js
@@ -1,17 +1,13 @@
 function start () {
 	$(".lang").on("click", changeLang);
+	$("#jobs li a").attr("href", "javascript:void(0);");
 	$("[data-translate]").jqTranslate('index');
-		$('#jobs').movingBoxes({
-		startPanel   : 1,      // start with this panel
-		reducedSize  : 0.8,    // non-current panel size: 80% of panel size
-		wrap         : true,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
-		buildNav     : true,   // if true, navigation links will be added
-		navFormatter : function(){ return "&#9679;"; } // function which returns the navigation text for each panel
-
-		// width and panelWidth options removed in v2.2.2, but still backwards compatible
-		// width        : 300,    // overall width of movingBoxes (not including navigation arrows)
-		// panelWidth   : 0.5,    // current panel width
-
+	$('#jobs').movingBoxes({
+	startPanel   : 1,      // start with this panel
+	reducedSize  : 0.8,    // non-current panel size: 80% of panel size
+	wrap         : true,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
+	buildNav     : true,   // if true, navigation links will be added
+	navFormatter : function(){ return "&#9679;"; } // function which returns the navigation text for each panel
 	});
 
 }
