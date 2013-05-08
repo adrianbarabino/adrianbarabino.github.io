a8f35ec588e9fafe7f2b59f5f5f4a012a0d01cde
Adrian Barabino <adrian.barabino@gmail.com>
2013-05-08 18:07:26 +0400

    style fix

--- js/site.js
+++ js/site.js
@@ -5,11 +5,11 @@ work_is_open = false;
 function open_work (info) {
 	var actual_work = this;
 	if(work_is_open){
-		$(".work").hide(400, function () {
+		$(".work").hide(10, function () {
 			$($(actual_work).attr("rel")).slideDown();
 		});
 	}else{
-		$("#workscontainer").slideDown(400, function () {
+		$("#workscontainer_inner").slideDown(400, function () {
 			$($(actual_work).attr("rel")).slideDown();
 		});
 	}
@@ -18,7 +18,7 @@ function open_work (info) {
 }
 function close_work (argument) {
 	$(".work").slideUp(400, function () {
-	$("#workscontainer").slideUp()});
+	$("#workscontainer_inner").slideUp()});
 	
 	work_is_open = false;
 }
@@ -30,7 +30,7 @@ function start () {
 		console.log(works_json);
 		$.each(works_json, function (i, val) {
 			$("#works").append('<li><a href="http://'+val.url+'" rel="#'+val.tag+'"><img src="/images/works/thumb/'+val.tag+'.jpg" alt=""><span>'+val.nombre+' ('+val.fecha+')</span></a></li>')
-			$("#workscontainer").append('<div class="work" style="display:none;" id="'+val.tag+'"><div class="close"></div>\n<figure>\n	<img src="/images/works/medium/'+val.tag+'.jpg" />\n</figure>\n<div class="details">\n  <h3>'+val.nombre+'</h3>\n  <h4>'+val.url+'</h4>\n  <span>Año</span>'+val.fecha+'<br>\n  <span>Mi trabajo ahí:</span>\n  <ul>\n  </ul>\n</div>\n</div>');
+			$("#workscontainer_inner").append('<div class="work" style="display:none;" id="'+val.tag+'"><div class="close"></div>\n<figure>\n	<img src="/images/works/medium/'+val.tag+'.jpg" />\n</figure>\n<div class="details">\n  <h3>'+val.nombre+'</h3>\n  <h4>'+val.url+'</h4>\n  <span>Año</span>'+val.fecha+'<br>\n  <span>Mi trabajo ahí:</span>\n  <ul>\n  </ul>\n</div>\n</div>');
 			$.each(val.trabajo, function (i, trabajo) {
 				$("div#"+val.tag+" div ul").append("<li>"+trabajo+"</li>");
 			})
