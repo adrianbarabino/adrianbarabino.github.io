2a470e2059919274114a0c7e0da0083bfa0ff0ca
Adrian Barabino <adrian.barabino@gmail.com>
2013-05-08 19:27:11 +0400

    style fix

--- js/site.js
+++ js/site.js
@@ -1,3 +1,16 @@
+// Feature detect + local reference
+var storage,
+    fail,
+    uid;
+try {
+  uid = new Date;
+  (storage = window.localStorage).setItem(uid, uid);
+  fail = storage.getItem(uid) != uid;
+  storage.removeItem(uid);
+  fail && (storage = false);
+} catch(e) {}
+
+
 var request = "";
 var works_json = "";
 var work_is_open;
@@ -39,9 +52,8 @@ function close_work (argument) {
 function start () {
 	$(".lang").on("click", changeLang);
 	$("[data-translate]").jqTranslate('index');
-	request = $.getJSON("trabajos.json", function (data) {
-		works_json = data;
-		console.log(works_json);
+	if (storage.works) {
+		works_json = JSON.parse(storage.works);
 		$.each(works_json, function (i, val) {
 			$("#works").append('<li><a href="http://'+val.url+'" rel="#'+val.tag+'"><img src="/images/works/thumb/'+val.tag+'.jpg" alt=""><span>'+val.nombre+' ('+val.fecha+')</span></a></li>')
 			$("#workscontainer_inner").append('<div class="work" style="display:none;" id="'+val.tag+'"><div class="close"></div>\n<figure>\n	<img src="/images/works/medium/'+val.tag+'.jpg" />\n</figure>\n<div class="details">\n  <h3>'+val.nombre+'</h3>\n  <h4>'+val.url+'</h4>\n  <span class="datew">'+val.fecha+'</span><br>\n  <span>Mi trabajo ahí:</span>\n  <ul>\n  </ul>\n</div>\n</div>');
@@ -50,6 +62,7 @@ function start () {
 			})
 			
 		})
+
 		$("#works li a").on("click", open_work);
 		$(".work .close").on("click", close_work);
 		$('#works').movingBoxes({
@@ -63,8 +76,38 @@ function start () {
 
 			$("#works li a").attr("href", "javascript:void(0);");
 		}, 500);
+	}else{
 		
-	});
+		request = $.getJSON("trabajos.json", function (data) {
+			works_json = data;
+			console.log(works_json);
+			$.each(works_json, function (i, val) {
+				$("#works").append('<li><a href="http://'+val.url+'" rel="#'+val.tag+'"><img src="/images/works/thumb/'+val.tag+'.jpg" alt=""><span>'+val.nombre+' ('+val.fecha+')</span></a></li>')
+				$("#workscontainer_inner").append('<div class="work" style="display:none;" id="'+val.tag+'"><div class="close"></div>\n<figure>\n	<img src="/images/works/medium/'+val.tag+'.jpg" />\n</figure>\n<div class="details">\n  <h3>'+val.nombre+'</h3>\n  <h4>'+val.url+'</h4>\n  <span class="datew">'+val.fecha+'</span><br>\n  <span>Mi trabajo ahí:</span>\n  <ul>\n  </ul>\n</div>\n</div>');
+				$.each(val.trabajo, function (i, trabajo) {
+					$("div#"+val.tag+" div ul").append("<li>"+trabajo+"</li>");
+				})
+				
+			})
+			if (storage) {
+				storage.setItem('works', JSON.stringify(works_json));
+			}
+			$("#works li a").on("click", open_work);
+			$(".work .close").on("click", close_work);
+			$('#works').movingBoxes({
+			startPanel   : 1,      // start with this panel
+			reducedSize  : 0.8,    // non-current panel size: 80% of panel size
+			wrap         : true,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
+			buildNav     : true,   // if true, navigation links will be added
+			navFormatter : function(){ return "&#9679;"; } // function which returns the navigation text for each panel
+			});
+			setTimeout(function () {
+
+				$("#works li a").attr("href", "javascript:void(0);");
+			}, 500);
+			
+		});
+	}
 	
 
 }
